abef8ad9c897131fcb9aa0358ea6dfa494cf2747
Lucas Munoz <lucas_munioz@hotmail.com>
2018-11-09 14:01:32 -0300

    Se hace estructura de carpetas, se renombra Form1 a frmPrincipal y se le cambia el texto

--- SistemaFER_v02/SistemaFER_v02/FORMS/frmPrincipal.cs
+++ SistemaFER_v02/SistemaFER_v02/FORMS/frmPrincipal.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SistemaFER_v02
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
